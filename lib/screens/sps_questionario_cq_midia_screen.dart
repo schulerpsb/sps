10868aa2cf1a1b5378726d14a32afdac50b3279a
Fernando Ferracini <fernandoferracini@gmail.com>
2020-11-19 10:33:54 -0300

    FERNANDO FERRACINI 19/11/2020 - 10:33

--- lib/screens/sps_questionario_cq_midia_screen.dart
+++ lib/screens/sps_questionario_cq_midia_screen.dart
@@ -14,127 +14,44 @@ class _sps_questionario_midia_screen extends State<sps_questionario_cq_midia_scr
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Color(0xFFe9eef7), // Cinza Azulado
-      appBar: AppBar(
-        backgroundColor: Color(0xFF004077),
-        title: Text(
-          'FOLLOW UP XPTO',
-          style: TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold),
-        ),
-        centerTitle: true,
-      ),
-      body: FutureBuilder<List<Map<String, dynamic>>>(
-        future: spsquestionariocqmidia.listarQuestionarioCqMidia(),
-        builder: (context, snapshot) {
-          debugPrint(snapshot.data.toString());
-          switch (snapshot.connectionState) {
-            case ConnectionState.none:
-              break;
-            case ConnectionState.waiting:
-              return Progress();
-              break;
-            case ConnectionState.active:
-              break;
-            case ConnectionState.done:
-              if (snapshot.hasError) {
-                var werror;
-                werror = snapshot.error.toString();
-                return CenteredMessage(
-                  'Falha de conexão! \n\n(' + werror + ')',
-                  icon: Icons.error,
-                );
-              }
-              if (snapshot.data.isNotEmpty) {
-                return ListView.builder(
-                  padding: EdgeInsets.only(top: 5),
-                  itemCount: snapshot.data.length,
-                  itemBuilder: (context, index) {
-                    return Card(
-                      color: Colors.white,
-                      child: ListTile(
-                        title: Text(
-                            '${snapshot.data[index]["codigo_programacao"]}' +
-                                " (" +
-                                snapshot.data[index]["status"] +
-                                ")",
-                            style: TextStyle(
-                                color: snapshot.data[index]["status"] == "OK"
-                                    ? Color(0xFF0bbf1a) // Verde
-                                    : snapshot.data[index]["status"] ==
-                                            "PARCIAL"
-                                        ? Color(0xFFfffb00) // Amarelo
-                                        : Colors.red,
-                                fontWeight: FontWeight.bold,
-                                fontSize: 20)),
-                        subtitle: Text(
-                            snapshot.data[index]["status"] == "PARCIAL"
-                                ? '${snapshot.data[index]["descr_programacao"]}' +
-                                    "\n\n" +
-                                    "PEDIDO: " +
-                                    '${snapshot.data[index]["codigo_pedido"]}' +
-                                    "/" +
-                                    '${snapshot.data[index]["item_pedido"]}' +
-                                    " (" +
-                                    '${snapshot.data[index]["codigo_material"]}' +
-                                    ")\n" +
-                                    "REFERÊNCIA: " +
-                                    '${snapshot.data[index]["referencia_parceiro"]}' +
-                                    "\nPROJETO: " +
-                                    '${snapshot.data[index]["codigo_projeto"]}' +
-                                    "\n\n" +
-                                    "PRAZO: " +
-                                    snapshot.data[index]["dtfim_aplicacao"] +
-                                    "        EVOLUÇÃO: " +
-                                    snapshot.data[index]["percentual_evolucao"]
-                                        .toStringAsPrecision(4)
-                                        .toString() +
-                                    " %"
-                                : '${snapshot.data[index]["descr_programacao"]}' +
-                                    "\n\n" +
-                                    "PEDIDO: " +
-                                    '${snapshot.data[index]["codigo_pedido"]}' +
-                                    "/" +
-                                    '${snapshot.data[index]["item_pedido"]}' +
-                                    " (" +
-                                    '${snapshot.data[index]["codigo_material"]}' +
-                                    ")\n" +
-                                    "REFERÊNCIA: " +
-                                    '${snapshot.data[index]["referencia_parceiro"]}' +
-                                    "\nPROJETO: " +
-                                    '${snapshot.data[index]["codigo_projeto"]}' +
-                                    "\n\n" +
-                                    "PRAZO: " +
-                                    snapshot.data[index]["dtfim_aplicacao"],
-                            style: TextStyle(
-                                color: Colors.black,
-                                fontWeight: FontWeight.bold)),
-                        trailing: Icon(Icons.open_in_new, color: Colors.black),
-                        onTap: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) =>
-                                    sps_questionario_cq_screen()),
-                            //Teste Adriano
-                            //sps_checklist_cq_screen(h_codigo_programacao: snapshot.data[index]["descr_programacao"])),
-                          );
-                        },
-                      ),
-                    );
-                  },
-                );
-              } else {
-                return CenteredMessage(
-                  'No transaction found!',
-                  icon: Icons.warning,
-                );
-              }
-              break;
-          }
-          return Text('Unkown error');
-        },
-      ),
-    );
+    return DefaultTabController(
+        length: 3,
+        child: new Scaffold(
+          backgroundColor: Color(0xFFe9eef7), // Cinza Azulado
+          appBar: AppBar(
+            backgroundColor: Color(0xFF004077),
+            title: Text(
+              'QUESTIONÁRIO - MÍDIA',
+              style: TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold),
+            ),
+            centerTitle: true,
+            bottom: TabBar(
+                tabs:
+                [
+                  Tab(icon: Icon(Icons.photo),),
+                  Tab(icon: Icon(Icons.video_library),),
+                  Tab(icon: Icon(Icons.audiotrack),),
+                ]
+
+            ),
+          ),
+          body: TabBarView(children:
+          [
+//             any widget can work very well here <3
+
+            new Container(
+              color: Color(0xFFe9eef7),
+              child: Center(child: Text('Hi from School', style: TextStyle(color: Colors.white),),),
+            ),
+            new Container(
+              color: Color(0xFFe9eef7),
+              child: Center(child: Text('Hi from home', style: TextStyle(color: Colors.white),),),
+            ),   new Container(
+            color: Color(0xFFe9eef7),
+            child: Center(child: Text('Hi from Hospital', style: TextStyle(color: Colors.white),),),
+          ),
+
+          ]),
+        ));
   }
 }
